e8d697303b2300d2a13eb21cdd80fe7b4b290ee2
PetarMPetrov <petrov.miroslavov@gmail.com>
2024-04-09 17:54:39 +0300

    repair the java class that find smallest and largest lexicographically substrings both with given length

--- FindFirstAndLastLexicographicallySubstring/FindSubstrings.java
+++ FindFirstAndLastLexicographicallySubstring/FindSubstrings.java
@@ -4,17 +4,34 @@ public class FindSubstrings {
 
 
         public static String getSmallestAndLargest(String s, int k) {
-            String smallest = "";
-            String largest = "";
 
             // Complete the function
             // 'smallest' must be the lexicographically smallest substring of length 'k'
             // 'largest' must be the lexicographically largest substring of length 'k'
 
+
+
+            String smallest = s.substring(0, k);
+            String largest = s.substring(0, k);
+
+            for (int i = 0; i <= s.length() - k; i++) {
+                String subString = s.substring(i, i + k);
+
+                if (subString.compareTo(smallest) < 0){
+                    smallest = subString;
+                }
+                if (subString.compareTo(largest) > 0){
+                    largest = subString;
+                }
+
+            }
+
+
+/*          first approach
+            String smallest = "";
+            String largest = "";
             int minIndex = 0;
             int maxIndex = 0;
-
-
             for (int i = 0; i < s.length(); i++) {
                 if((int)s.charAt(i) < s.charAt(minIndex)){
                     minIndex = i;
@@ -27,8 +44,6 @@ public class FindSubstrings {
                         }
                     }
                 }
-
-
                 if((int)s.charAt(i) > s.charAt(maxIndex)){
                     maxIndex = i;
                 }
@@ -40,15 +55,12 @@ public class FindSubstrings {
                         }
                     }
                 }
-
-
             }
-
             for (int i = 0; i < k; i++) {
                 smallest = smallest + s.charAt(minIndex + i);
                 largest = largest + s.charAt(maxIndex + i);
             }
-
+*/
 
 
             return smallest + "\n" + largest;
