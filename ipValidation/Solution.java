9af65e2793e440472e44c4f72c4b2b3deee20b7a
PetarMPetrov <petrov.miroslavov@gmail.com>
2024-04-20 18:37:44 +0300

    added class for ip validation. the pattern adjustment still in progress

---
+++ ipValidation/Solution.java
@@ -0,0 +1,29 @@
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class Solution{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+//Write your code here
+
+class MyRegex {
+
+    String pattern = "";
+
+
+    public boolean pattern(String IP){
+
+
+        return Pattern.matches(pattern, IP);
+    }
+
+}
