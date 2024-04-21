d1aececd4a079101b907a295b79b89cf4f255206
PetarMPetrov <petrov.miroslavov@gmail.com>
2024-04-21 12:55:21 +0300

    ip validation done!

--- ipValidation/Solution.java
+++ ipValidation/Solution.java
@@ -17,7 +17,7 @@ class Solution{
 
 class MyRegex {
 
-    String pattern = "";
+    String pattern = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
 
 
     public boolean pattern(String IP){
