58987eba5b5d364d11744b8230325427326b760b
PetarMPetrov <petrov.miroslavov@gmail.com>
2024-04-25 18:13:48 +0300

    finished program for deleting duplicate words

--- RemovingDuplicateWords/DuplicateWords.java
+++ RemovingDuplicateWords/DuplicateWords.java
@@ -6,8 +6,10 @@ public class DuplicateWords {
 
     public static void main(String[] args) {
 
-        String regex = "/* Write a RegEx matching repeated words here. */";
-        Pattern p = Pattern.compile(regex, /* Insert the correct Pattern flag here.*/);
+        //"/* Write a RegEx matching repeated words here. */"
+        String regex = "\\b(\\w+)(?:\\s+\\1\\b)+";
+        /* Insert the correct Pattern flag here.*/
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
 
         Scanner in = new Scanner(System.in);
         int numSentences = Integer.parseInt(in.nextLine());
@@ -19,7 +21,8 @@ public class DuplicateWords {
 
             // Check for subsequences of input that match the compiled pattern
             while (m.find()) {
-                input = input.replaceAll(/* The regex to replace */, /* The replacement. */);
+                /* The regex to replace */ /* The replacement. */
+                input = input.replaceAll(m.group(), m.group(1));
             }
 
             // Prints the modified sentence.
